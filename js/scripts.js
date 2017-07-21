471968a4ee8c315a6f1d5e31660ffe32950e3439
Collin <pineapplepie4@yahoo.com>
2017-07-21 12:51:39 -0700

    have mutiple options to choice from and diffrent ways of picking your toppings.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//buisness logic
+function pizza(toppings,size,cost)  {
+  this.topping = topping;
+  this.size = size
+  this.sauce =sauce
+};
+pizza.prototype.newPizza = (function()  {
+  return this.pizza + this.size + this.topping;
+});
+
+
+
+//user interface
+$(document).ready(function(){
+  var pizzaSizeCost = parseInt("#size").val();
+
+  $("#pizza-menu").submit(function(event){
+    event.preventDefault();
+
+    // var pizzaSauceCost = $("input:radio[name=flavor]:checked").val();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var pizzaToppingCost = parseInt($(this).val());
+
+      $('#pizza-responses').append(pizzaToppingCost + "<br>");
+      console.log(pizzaToppingCost)
+      var totalPrice = new price (pizzaToppingCost, pizzaSizeCost, pizzaSauceCost);
+
+    });
+
+    console.log(pizzaToppingCost)
+
+    // $('#transportation_survey').hide();
+  });
+});
